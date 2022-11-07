90dc4907d782de8cc4c802d79edd2ca7c7b03fae
ByeongbumSeo <bum4321@naver.com>
2022-11-07 21:05:19 +0900

    [init] First Commit

---
+++ .github/ISSUE_TEMPLATE/feature.md
@@ -0,0 +1,16 @@
+---
+name: Feature
+about: Feature 작업 사항을 입력해주세요!
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+## 설명
+
+## To-Do
+- [ ] 
+- [ ] 
+
+## ETC
---
+++ src/main/java/com/sparta/finalpj/FinalpjApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.finalpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalpjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalpjApplication.class, args);
+    }
+
+}
