36d4c24a0a747b36093c9befe1fc72701d2c05b8
bokyungkim <bkyungkeem@daum.net>
2022-11-22 22:58:12 +0900

    feat : 내일정(캘린더) 기능 추가
    fix: 에러코드 추가
    fix: 내명함/자사,타사명함추가 3차 수정

---
+++ src/main/java/com/sparta/finalpj/controller/MyCalendarController.java
@@ -0,0 +1,40 @@
+package com.sparta.finalpj.controller;
+
+import com.sparta.finalpj.controller.request.calendar.MyCalendarRequestDto;
+import com.sparta.finalpj.controller.response.ResponseDto;
+import com.sparta.finalpj.service.MyCalendarService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/api")
+public class MyCalendarController {
+    private final MyCalendarService myCalendarService;
+
+    // 내일정 등록
+    @PostMapping(value = "/calendar")
+    public ResponseDto<?> createCalendar(@RequestBody MyCalendarRequestDto requestDto, HttpServletRequest request) {
+        return myCalendarService.createCalendar(requestDto, request);
+    }
+
+    // 내일정 수정
+    @PutMapping(value = "/calendar/{calendarId}")
+    public ResponseDto<?> updateCalendar(@PathVariable Long calendarId, @RequestBody MyCalendarRequestDto requestDto, HttpServletRequest request) {
+        return myCalendarService.updateCalendar(calendarId, requestDto, request);
+    }
+
+    // 내일정 삭제
+    @DeleteMapping(value = "/calendar/{calendarId}")
+    public ResponseDto<?> deleteCalendar(@PathVariable Long calendarId, HttpServletRequest request) {
+        return myCalendarService.deleteCalendar(calendarId, request);
+    }
+
+    // 내일정 전체조회
+    @GetMapping(value = "/calendar")
+    public ResponseDto<?> getAllCalendar(HttpServletRequest request) {
+        return myCalendarService.getAllCalendar(request);
+    }
+}
---
+++ src/main/java/com/sparta/finalpj/controller/response/calendar/MyCalendarResponseDto.java
@@ -0,0 +1,25 @@
+package com.sparta.finalpj.controller.response.calendar;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyCalendarResponseDto {
+    private Long id;
+    private String startDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
+    private String filteredDate;
+    private String title;
+    private String todo;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
