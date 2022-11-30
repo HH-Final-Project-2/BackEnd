e28568a73fb5183e85168558d016b2dfe280e18a
HelloWorld <58963042+kimbokyung1220@users.noreply.github.com>
2022-11-30 21:44:32 +0900

    Delete CompanyInfoRepository.java

--- src/main/java/com/sparta/finalpj/repository/CompanyInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.finalpj.repository;
-
-import com.sparta.finalpj.domain.CompanyInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyInfoRepository extends JpaRepository<CompanyInfo, Long> {
-}
