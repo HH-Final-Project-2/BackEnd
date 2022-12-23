9179ab3bde0041a49813b4d2fe3e5dace3b7479d
yusung4612 <jin731314@gmail.com>
2022-12-23 15:11:53 +0900

    remove : Google Storage 사용으로 S3 관련 파일 제거

--- src/main/java/com/sparta/finalpj/exception/EmptyMultipartFileException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.finalpj.exception;
-
-public class EmptyMultipartFileException extends RuntimeException {
-  public EmptyMultipartFileException() {
-    super("multipart file is empty");
-  }
-}
--- src/main/java/com/sparta/finalpj/exception/RemoveFileException.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.finalpj.exception;
-
-public class RemoveFileException extends RuntimeException {
-  public RemoveFileException() {
-    super("fail to remove target file");
-  }
-}
