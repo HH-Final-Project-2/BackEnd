c928d69ba7cdc9a23c1559be7388b7103744ea3e
yusung4612 <jin731314@gmail.com>
2022-12-28 04:58:09 +0900

    feat : MemberControllerTest Builder 추가

--- src/main/java/com/sparta/finalpj/controller/request/member/LoginRequestDto.java
+++ src/main/java/com/sparta/finalpj/controller/request/member/LoginRequestDto.java
@@ -1,7 +1,9 @@
 package com.sparta.finalpj.controller.request.member;
 
+import lombok.Builder;
 import lombok.Getter;
 
+@Builder
 @Getter
 public class LoginRequestDto {
     private String email;
--- src/main/java/com/sparta/finalpj/controller/request/member/SignupRequestDto.java
+++ src/main/java/com/sparta/finalpj/controller/request/member/SignupRequestDto.java
@@ -1,10 +1,11 @@
 package com.sparta.finalpj.controller.request.member;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
-
+@Builder
 @Getter
 public class SignupRequestDto {
     private String username;
